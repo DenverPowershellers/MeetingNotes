ef0c501b834c32ee7752fb251af8206939a7e247
joeypiccola <joey.piccola@gmail.com>
2018-09-14 09:31:14 -0600

    add ansible powershell talk

---
+++ 08.06.18 - Ansible + PowerShell/bootstrap_winrm.ps1
@@ -0,0 +1,8 @@
+# reference: https://docs.ansible.com/ansible/2.6/user_guide/windows_setup.html
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
